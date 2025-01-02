d12536228d8c02414b6113cdc7c9b6c86b5b3fce
Sasivisvan <80504986+Sasivisvan@users.noreply.github.com>
2025-01-02 23:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// Retrieve todo from local storage or initialize an empty array
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+
+// Initialize
+document.addEventListener("DOMContentLoaded", function () {
+  addButton.addEventListener("click", addTask);
+  todoInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevents default Enter key behavior
+      addTask();
+    }
+  });
+  deleteButton.addEventListener("click", deleteAllTasks);
+  displayTasks();
+});
+
+function addTask() {
+  const newTask = todoInput.value.trim();
+  const Date = date.value.trim();
+  const Time = time.value.trim();
+  if (newTask !== "") {
+    todo.push({ text: newTask, disabled: false, date: Date, time: Time });
+    saveToLocalStorage();
+    todoInput.value = "";
+    displayTasks();
+  }
+}
+
+function displayTasks() {
+  todoList.innerHTML = "";
+  todo.forEach((item, index) => {
+    const p = document.createElement("p");
+    p.innerHTML = `
+      <div class="todo-container">
+        <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+      item.disabled ? "checked" : ""
+    }>
+        <p id="todo-${index}" class="${
+      item.disabled ? "disabled" : ""
+    }" onclick="editTask(${index})">${item.text}  ${item.date}  ${item.time}</p>
+      </div>
+    `;
+    p.querySelector(".todo-checkbox").addEventListener("change", () =>
+      toggleTask(index)
+    );
+    todoList.appendChild(p);
+  });
+  todoCount.textContent = todo.length;
+}
+
+function editTask(index) {
+  const todoItem = document.getElementById(`todo-${index}`);
+  const existingText = todo[index].text;
+  const inputElement = document.createElement("input");
+
+  inputElement.value = existingText;
+  todoItem.replaceWith(inputElement);
+  inputElement.focus();
+
+  inputElement.addEventListener("blur", function () {
+    const updatedText = inputElement.value.trim();
+    if (updatedText) {
+      todo[index].text = updatedText;
+      saveToLocalStorage();
+    }
+    displayTasks();
+  });
+}
+
+function toggleTask(index) {
+  todo[index].disabled = !todo[index].disabled;
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function deleteAllTasks() {
+  const userResponse = confirm(
+    "Warning! Are you sure you want to delete all tasks"
+  );
+
+  if (userResponse) {
+    todo = [];
+    saveToLocalStorage();
+    displayTasks();
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("todo", JSON.stringify(todo));
+}
